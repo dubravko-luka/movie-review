c1573a40d73696ea0626d615f470b3fadbc040fa
Phuong Nguyen <dev.richard.npm98@gmail.com>
2023-06-22 08:56:06 +0700

    change element a to Link in 404.tsx

--- src/api/graphql/queries/index.ts
+++ src/api/graphql/queries/index.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://app.reshare.com.au:8085/graphql',
+  uri: 'https://app.reshare.com.au/graphql',
   cache: new InMemoryCache(),
 });
 
